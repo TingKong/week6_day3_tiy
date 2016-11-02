72d558efd5f9ba058f1a53474e4d9f9aed38b391
Ting <Ting@DESKTOP-MJSRLVH>
2016-11-02 13:41:57 -0400

    pushing files up

---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many times would you like the Fibonacci to run?");
+            string answer = Console.ReadLine();
+            int numAnswer = Convert.ToInt32(answer);
+            
+
+            Console.ReadLine();
+        }
+
+        static void Fibby(int a, int b, int count)
+        {
+
+        }
+        {
+            
+        }
+}
