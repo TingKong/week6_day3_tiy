354735da06e37666adb0dc38a2020315d3789f51
Ting <Ting@DESKTOP-MJSRLVH>
2016-11-02 17:28:52 -0400

    creating for loop

--- Fibonacci/Fibonacci/Program.cs
+++ Fibonacci/Fibonacci/Program.cs
@@ -10,19 +10,82 @@ namespace Fibonacci
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("How many times would you like the Fibonacci to run?");
-            string answer = Console.ReadLine();
-            int numAnswer = Convert.ToInt32(answer);
-            
+            //FIBONACCI FORMULA
+            //Console.WriteLine("How many times would you like the Fibonacci to run?");
+            //string answer = Console.ReadLine();
+            //int numAnswer = Convert.ToInt32(answer);
+
+
+
+            //Fibby(0, 1, 1, numAnswer);
+            //Console.ReadLine();
+
+            //FACTORIALS
+            Console.WriteLine("What number would you want to fractor?");
+            string useranswer = Console.ReadLine();
+            int numUserAns = Convert.ToInt32(useranswer);
+
+
+            int final = Fact(numUserAns);
+
+            for (int i = numUserAns; i > 0; i--)
+            {
+                Console.Write(i + " x ");
+                Console.WriteLine();
+                for (int b = numUserAns; b > 0; b--)
+                {
+                    Console.Write(b + " x ");
+
+                }
+            }
 
             Console.ReadLine();
         }
 
-        static void Fibby(int a, int b, int count)
+        //static void Fibby(int a, int b, int count, int number)
+        //{
+
+        //    Console.WriteLine(a);
+        //    if(count < number)
+        //    {
+        //        Fibby(b, a + b, count+1, number);
+        //    }
+
+
+        //}
+
+        static int Fact(int a)
         {
 
+            if (a > 0)
+            {
+
+                return a * Fact(a - 1);
+
+            }
+
+            return 1;
+
         }
+
+        static void giveBack(int num, int a)
         {
-            
+         
+            for (int i = 1; i < num + 1; i++)
+            {
+                if (i == num)
+                {
+                    Console.Write(i + " = " + a);
+                }
+                else
+                {
+                    Console.Write(i + " x ");
+                }
+
+            }
         }
+
+
+    }
+
 }
